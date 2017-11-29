3f9044afb190b717da0324295af2d36ddfaa2513
Aaron Hartigan <hartiganaaron@gmail.com>
2017-11-29 12:55:48 -0800

    added whitespace to tests

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -12,18 +12,22 @@ describe('<Search />', () => {
     fetch: () => {},
   };
   const wrapper = shallow(<SearchTest context={context} />);
+
   test('Should initially display loading spinner', () => {
     expect(wrapper.state('isLoading')).toBeTruthy();
     expect(wrapper.find(Spinner).length).toBe(1);
   });
+
   test('Should not display loading spinner if not isLoading', () => {
     wrapper.setState({ isLoading: false });
     expect(wrapper.state('isLoading')).toBeFalsy();
     expect(wrapper.find(Spinner).length).toBe(0);
   });
+
   test('Should not display GoogleMap if coordinates are null', () => {
     expect(wrapper.find(GoogleMapReact).length).toBe(0);
   });
+
   test('Should display GoogleMap if coordinates are numbers', () => {
     wrapper.setState({ lat: 1, long: 1 });
     expect(wrapper.find(GoogleMapReact).length).toBe(1);
