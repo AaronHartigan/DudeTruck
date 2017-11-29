2ac576174e397e2897f4cbf841bfc79c3948c94a
Aaron Hartigan <hartiganaaron@gmail.com>
2017-11-29 13:42:15 -0800

    Added skeleton code for reviews

--- src/components/Vendor/Vendor.js
+++ src/components/Vendor/Vendor.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Vendor.css';
 
-const hasFoodInEnglish = function hasFoodInEnglish(hasFood) {
+const boolToEnglish = function boolToEnglish(hasFood) {
   return hasFood ? 'Yes' : 'No';
 };
 
@@ -21,6 +21,14 @@ class Vendor extends React.Component {
       vegetarian: PropTypes.bool.isRequired,
       glutenFree: PropTypes.bool.isRequired,
     }),
+    reviews: PropTypes.arrayOf(
+      PropTypes.shape({
+        text: PropTypes.string,
+        rating: PropTypes.number,
+        reviewerName: PropTypes.string,
+        reviewerAge: PropTypes.number,
+      }),
+    ),
     error: PropTypes.bool,
   };
 
@@ -37,8 +45,40 @@ class Vendor extends React.Component {
       vegetarian: false,
       glutenFree: false,
     },
+    reviews: [],
     error: true,
   };
+
+  constructor() {
+    super();
+
+    this.state = {
+      review: '',
+      rating: null,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+
+    // this will change (just to get eslint off my back)
+    this.setState({
+      rating: null,
+    });
+  }
 
   render() {
     if (this.props.error) {
@@ -48,19 +88,36 @@ class Vendor extends React.Component {
         </div>
       );
     }
+
+    const reviews = this.props.reviews.map(review => <div>{review.text}</div>);
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <img src={this.props.truck.logo} alt="logo" />
+          <img src={this.props.truck.logo} alt="" />
           <h1>{this.props.truck.companyName}</h1>
           <div>{this.props.truck.description}</div>
           <div>Phone: {this.props.truck.phone}</div>
           <div>Schedule: {this.props.truck.schedule}</div>
-          <div>Vegan: {hasFoodInEnglish(this.props.truck.vegan)}</div>
-          <div>Vegetarian: {hasFoodInEnglish(this.props.truck.vegetarian)}</div>
-          <div>
-            Gluten Free: {hasFoodInEnglish(this.props.truck.glutenFree)}
-          </div>
+          <div>Vegan: {boolToEnglish(this.props.truck.vegan)}</div>
+          <div>Vegetarian: {boolToEnglish(this.props.truck.vegetarian)}</div>
+          <div>Gluten Free: {boolToEnglish(this.props.truck.glutenFree)}</div>
+          <form onSubmit={this.handleSubmit}>
+            <div className={s.form}>
+              <label htmlFor="review">
+                <textarea
+                  className={s.formControlTextArea}
+                  type="text"
+                  name="review"
+                  id="review"
+                  placeholder="Leave a review..."
+                  value={this.state.review}
+                  onChange={this.handleChange}
+                />
+              </label>
+            </div>
+            <input className={s.button} type="submit" value="Submit" />
+          </form>
+          <div>{reviews}</div>
         </div>
       </div>
     );
